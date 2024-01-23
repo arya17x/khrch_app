e7d8cbf40a04f3cde68e8e9461df607619b21fd0
arya17xx <aryavishal2002@gmail.com>
2024-01-24 00:27:08 +0530

    initial commit

---
+++ lib/lunchpage.dart/lunchScreen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class LunchScreen extends StatelessWidget {
+  const LunchScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Center(
+        child: Container(
+          child: Text("Arya is very Good"),
+        ),
+      ),
+    );
+  }
+}
